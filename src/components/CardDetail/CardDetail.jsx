103d68861668c0eca068cc3b912bee2ade7f45c5
Rocío Cejudo <rocio.cejudo.soronellas@gmail.com>
2024-11-04 19:50:55 +0100

    add and delete functionalities for version 2 assessment

--- src/components/CardDetail/CardDetail.jsx
+++ src/components/CardDetail/CardDetail.jsx
@@ -1,3 +1,4 @@
+import ShowMore from "../ShowMoreCardDetail/ShowMore"
 import "./CardDetail.css"
 
 function CardDetail({ card }) {
@@ -7,13 +8,12 @@ function CardDetail({ card }) {
         <img src={card.imageUrl} alt={card.name} />
         <h2>{card.name}</h2>
         <div className="type">
-          <h3>Type</h3>
           <p>{card.type}</p>
         </div>
-        <div className="description">
-          <h3>Description</h3>
-          <p>{card.text}</p>
+        <div className="setName">
+          <p>{card.setName}</p>
         </div>
+        <ShowMore/>
       </div>
     ) : null
   )
---
+++ src/components/ShowMoreCardDetail/ShowMore.jsx
@@ -0,0 +1,8 @@
+import "./ShowMore.css"
+function ShowMore() {
+  return (
+    <button className='btn-showMore'>Show More</button>
+  )
+}
+
+export default ShowMore
